5c4a907b3db4438d5ce4bb8ecda5f37ea44d6d57
ZokirovM <jutsu849@gmail.com>
2025-09-13 19:54:20 +0500

    Create  main.py
    versia 1.0

---
+++ main.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+conn=sqlite3.connect('test.db')
+cursor=conn.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS students (id INT, surname TEXT, name TEXT,'
+               'birthday INT, gendor TEXT, faculty TEXT)')
+#
+# cursor.execute("INSERT INTO students (id, surname, name,"
+#                "birthday, gendor, faculty) VALUES ( 0, 'Ismoil', 'BF')")
+# cursor.execute("INSERT INTO students (id, surname, name,"
+#                "birthday, gendor, faculty) VALUES ( 1, 'Islom', 'CF')")
+# cursor.execute("INSERT INTO students (id, surname, name,"
+#                "birthday, gendor, faculty) VALUES ( 2, 'Xasan', 'CF')")
+# cursor.execute("INSERT INTO students (id, surname, name,"
+#                "birthday, gendor, faculty) VALUES ( 3, 'Ibrohim',  'CF')")
+# cursor.execute("INSERT INTO students (id, surname, name,"
+#                "birthday, gendor, faculty) VALUES ( 4, 'Nigina', 'Davronova', 'BF')")
+#
+# cursor.execute('''SELECT * FROM students''')
+# result=cursor.fetchall()
+# print(result)
+#
+#
+cursor.execute('''SELECT * FROM students WHERE grades > 4''')
+
+rows=cursor.fetchall()
+for row in rows:
+    print(row)
+#
+#
+# cursor.execute('''SELECT * FROM students ORDER BY name''')
+#
+# result=cursor.fetchall()
+# print(result)
+#
+
+conn.commit()
+
+
